faab68fc85f997efe2f909c810d2900b0236867e
hugoCanul <51390737+hcanul@users.noreply.github.com>
2023-08-11 13:35:33 -0500

    commit 1.35

--- app/Http/Livewire/Asesor/AsesorController.php
+++ app/Http/Livewire/Asesor/AsesorController.php
@@ -106,8 +106,17 @@ class AsesorController extends Component
             $data = Adviser::orderBy('id', 'asc')->paginate($this->pagination);
         }
 
+        if ($this->city_id)
+        {
+            $asesores = Employes::whereCityId($this->city_id)->whereTypeemployesId(2)->get();
+        }
+        else
+        {
+            $asesores = Employes::all();
+        }
+
         $sucursales = Cities::all();
-        $asesores = Employes::whereTypeemployesId(2)->get();
+        // $asesores = Employes::whereTypeemployesId(2)->get();
 
         return view('livewire.asesor.asesor', [
             'data' => $data,
@@ -118,133 +127,124 @@ class AsesorController extends Component
             ->section('content');
     }
 
-    public function resetUI()
+    // public function resetUI()
+    // {
+    //     $this->city_id = null;
+    //     $this->employes_id = null;
+    //     $this->clientesi = null;
+    //     $this->carterainicio = null;
+    //     $this->srinicio = null;
+    //     $this->porsrinicio = null;
+    //     $this->clientesf = null;
+    //     $this->carterafinal = null;
+    //     $this->srfinal = null;
+    //     $this->porsrfinal = null;
+    //     $this->category_adviser_id = null;
+    //     $this->metacoloca = null;
+    //     $this->colocadoreal = null;
+    //     $this->poralcancemetacoloca = null;
+    //     $this->diferenciaclientes = null;
+    //     $this->diferenciacartera = null;
+    //     $this->bonoclientes = null;
+    //     $this->bonoccolocacion = null;
+    //     $this->bonoexcelencia = null;
+    //     $this->bonofina = null;
+    //     $this->search = '';
+    //     $this->selected_id = 0;
+    //     $this->pageTitle = 'Listado';
+    //     $this->componentName = 'CÁLCULO ASESOR';
+    // }
+
+    // public function Store()
+    // {
+    //     $this->validate($this->rules, $this->messages);
+
+    //     Administrators::create([
+    //         'city_id' => $this->city_id,
+    //         'employes_id' => $this->employes_id,
+    //         'asesores' => $this->asesores,
+    //         'typesucursal' => $this->typesucursal,
+    //         'category_id' => session('categoria')->id,
+    //         'clientesi' => $this->clientesi,
+    //         'carterainicio' => $this->carterainicio,
+    //         'clientesf' => $this->clientesf,
+    //         'colocadoreal' => $this->colocadoreal,
+    //         'diferenciaclientes' => $this->diferenciaclientes,
+    //         'bonoclientes' => $this->bonoclientes,
+    //         'bonoccolocacion' => $this->bonoccolocacion,
+    //         'bonofina' => $this->bonofina,
+    //     ]);
+
+    //     $this->resetUI();
+    //     session()->flash('message', 'Calculo Anexado con exito!');
+    //     $this->emit('item-added', 'Calculo realizado registrada!');
+    // }
+
+    // public function Editar($id)
+    // {
+    //     $administrator = Administrators::find($id);
+
+    //     $this->city_id = $administrator->city_id;
+    //     $this->employes_id = $administrator->employes_id;
+    //     $this->asesores = $administrator->asesores;
+    //     $this->typesucursal = $administrator->typesucursal;
+    //     $this->category_id = $administrator->category_id;
+    //     $this->clientesi = $administrator->clientesi;
+    //     $this->carterainicio = $administrator->carterainicio;
+    //     $this->clientesf = $administrator->clientesf;
+    //     $this->colocadoreal = $administrator->colocadoreal;
+    //     $this->diferenciaclientes = $administrator->diferenciaclientes;
+    //     $this->bonoclientes = $administrator->bonoclientes;
+    //     $this->bonoccolocacion = $administrator->bonoccolocacion;
+    //     $this->bonofina = $administrator->bonofina;
+
+    //     $this->selected_id = $id;
+    // }
+
+    // public function Update()
+    // {
+    //     $this->validate($this->rules, $this->messages);
+
+    //     $admini = Administrators::find($this->selected_id);
+
+    //     $admini->update([
+    //         'city_id' => $this->city_id,
+    //         'employes_id' => $this->employes_id,
+    //         'asesores' => $this->asesores,
+    //         'typesucursal' => $this->typesucursal,
+    //         'category_id' => $this->category_id,
+    //         'clientesi' => $this->clientesi,
+    //         'carterainicio' => $this->carterainicio,
+    //         'clientesf' => $this->clientesf,
+    //         'colocadoreal' => $this->colocadoreal,
+    //         'diferenciaclientes' => $this->diferenciaclientes,
+    //         'bonoclientes' => $this->bonoclientes,
+    //         'bonoccolocacion' => $this->bonoccolocacion,
+    //         'bonofina' => $this->bonofina,
+    //     ]);
+
+    //     $this->resetUI();
+    //     session()->flash('message', 'Calculo Editada con exito!');
+    //     $this->emit('item-updated', 'Calculo Actualizada');
+    // }
+
+    // public function Destroy(Administrators $administrator)
+    // {
+    //     $administrator->delete();
+    //     session()->flash('delete', 'Calculo Eliminado con exito!');
+    //     $this->emit('item-deleted', 'Calculo Eliminado con exito');
+    //     $this->resetUI();
+    // }
+
+    public function psr()
     {
-        $this->city_id = null;
-        $this->employes_id = null;
-        $this->clientesi = null;
-        $this->carterainicio = null;
-        $this->srinicio = null;
-        $this->porsrinicio = null;
-        $this->clientesf = null;
-        $this->carterafinal = null;
-        $this->srfinal = null;
-        $this->porsrfinal = null;
-        $this->category_adviser_id = null;
-        $this->metacoloca = null;
-        $this->colocadoreal = null;
-        $this->poralcancemetacoloca = null;
-        $this->diferenciaclientes = null;
-        $this->diferenciacartera = null;
-        $this->bonoclientes = null;
-        $this->bonoccolocacion = null;
-        $this->bonoexcelencia = null;
-        $this->bonofina = null;
-        $this->search = '';
-        $this->selected_id = 0;
-        $this->pageTitle = 'Listado';
-        $this->componentName = 'CÁLCULO ASESOR';
-    }
-
-    public function diferencia()
-    {
-        $this->diferenciaclientes = $this->clientesf - $this->clientesi;
-
-        if($this->diferenciaclientes < 0)
-            $this->diferenciaclientes = 0;
-
-        $this->bonoclientes = $this->diferenciaclientes * session('categoria')->pagocrecliente;
-
-        $this->emit('dife',[$this->diferenciaclientes, $this->bonoclientes]);
-    }
-
-    public function coloca()
-    {
-        $this->bonoccolocacion = $this->colocadoreal * session('categoria')->porcpago;
-
-        $this->bonofina = $this->bonoclientes + $this->bonoccolocacion;
-
-        $this->emit('colocado', [$this->bonoccolocacion, $this->bonofina]);
-    }
-
-    public function Store()
-    {
-        $this->validate($this->rules, $this->messages);
-
-        Administrators::create([
-            'city_id' => $this->city_id,
-            'employes_id' => $this->employes_id,
-            'asesores' => $this->asesores,
-            'typesucursal' => $this->typesucursal,
-            'category_id' => session('categoria')->id,
-            'clientesi' => $this->clientesi,
-            'carterainicio' => $this->carterainicio,
-            'clientesf' => $this->clientesf,
-            'colocadoreal' => $this->colocadoreal,
-            'diferenciaclientes' => $this->diferenciaclientes,
-            'bonoclientes' => $this->bonoclientes,
-            'bonoccolocacion' => $this->bonoccolocacion,
-            'bonofina' => $this->bonofina,
-        ]);
-
-        $this->resetUI();
-        session()->flash('message', 'Calculo Anexado con exito!');
-        $this->emit('item-added', 'Calculo realizado registrada!');
-    }
-
-    public function Editar($id)
-    {
-        $administrator = Administrators::find($id);
-
-        $this->city_id = $administrator->city_id;
-        $this->employes_id = $administrator->employes_id;
-        $this->asesores = $administrator->asesores;
-        $this->typesucursal = $administrator->typesucursal;
-        $this->category_id = $administrator->category_id;
-        $this->clientesi = $administrator->clientesi;
-        $this->carterainicio = $administrator->carterainicio;
-        $this->clientesf = $administrator->clientesf;
-        $this->colocadoreal = $administrator->colocadoreal;
-        $this->diferenciaclientes = $administrator->diferenciaclientes;
-        $this->bonoclientes = $administrator->bonoclientes;
-        $this->bonoccolocacion = $administrator->bonoccolocacion;
-        $this->bonofina = $administrator->bonofina;
-
-        $this->selected_id = $id;
-    }
-
-    public function Update()
-    {
-        $this->validate($this->rules, $this->messages);
-
-        $admini = Administrators::find($this->selected_id);
-
-        $admini->update([
-            'city_id' => $this->city_id,
-            'employes_id' => $this->employes_id,
-            'asesores' => $this->asesores,
-            'typesucursal' => $this->typesucursal,
-            'category_id' => $this->category_id,
-            'clientesi' => $this->clientesi,
-            'carterainicio' => $this->carterainicio,
-            'clientesf' => $this->clientesf,
-            'colocadoreal' => $this->colocadoreal,
-            'diferenciaclientes' => $this->diferenciaclientes,
-            'bonoclientes' => $this->bonoclientes,
-            'bonoccolocacion' => $this->bonoccolocacion,
-            'bonofina' => $this->bonofina,
-        ]);
-
-        $this->resetUI();
-        session()->flash('message', 'Calculo Editada con exito!');
-        $this->emit('item-updated', 'Calculo Actualizada');
-    }
-
-    public function Destroy(Administrators $administrator)
-    {
-        $administrator->delete();
-        session()->flash('delete', 'Calculo Eliminado con exito!');
-        $this->emit('item-deleted', 'Calculo Eliminado con exito');
-        $this->resetUI();
+        if ($this->carterainicio > 0 && $this->srinicio > 0)
+        {
+            $this->porsrinicio = $this->srinicio / $this->carterainicio;
+        }
+        else
+        {
+            $this->porsrinicio = 0;
+        }
     }
 }
--- resources/views/livewire/asesor/form.blade.php
+++ resources/views/livewire/asesor/form.blade.php
@@ -35,14 +35,14 @@
             </div>
             <div>
                 <label for="carterainicio" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Cartera Inicio</label>
-                <input type="text" wire:model='carterainicio' id="carterainicio" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0" disabled>
+                <input type="text" wire:model='carterainicio' id="carterainicio" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0">
                 @error('carterainicio')
                     <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">{{ $message }}</span></p>
                 @enderror
             </div>
             <div>
                 <label for="srinicio" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">SR Inicio</label>
-                <input type="number" wire:model='srinicio' id="srinicio" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0">
+                <input type="number" wire:model='srinicio' id="srinicio" wire:keydown.enter='psr' class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0">
                 @error('srinicio')
                     <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">{{ $message }}</span></p>
                 @enderror
@@ -59,6 +59,13 @@
                 <label for="clientesf" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Clientes Final</label>
                 <input type="number" wire:model='clientesf' id="clientesf" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0">
                 @error('clientesf')
+                    <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">{{ $message }}</span></p>
+                @enderror
+            </div>
+            <div>
+                <label for="carterafinal" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Cartera Final</label>
+                <input type="number" wire:model='clientesf' id="carterafinal" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="0">
+                @error('carterafinal')
                     <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">{{ $message }}</span></p>
                 @enderror
             </div>
